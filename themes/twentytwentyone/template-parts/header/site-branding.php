3d63cdab066c4d8f623f8a49d0ff7df9be201d0d
mnatkanski <90765931+michalnatkanski@users.noreply.github.com>
2023-10-11 18:32:35 +0200

    Update header branding (#3)

--- themes/twentytwentyone/template-parts/header/site-branding.php
+++ themes/twentytwentyone/template-parts/header/site-branding.php
@@ -7,36 +7,19 @@
  * @since Twenty Twenty-One 1.0
  */
 
-$blog_info    = get_bloginfo( 'name' );
-$description  = get_bloginfo( 'description', 'display' );
-$show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
-$header_class = $show_title ? 'site-title' : 'screen-reader-text';
-
 ?>
-
-<?php if ( has_custom_logo() && $show_title ) : ?>
-	<div class="site-logo"><?php the_custom_logo(); ?></div>
-<?php endif; ?>
 
 <div class="site-branding">
 
-	<?php if ( has_custom_logo() && ! $show_title ) : ?>
+	<?php if ( has_custom_logo() ) : ?>
+		<svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g opacity="0.3">
+			<circle cx="1.5" cy="1.5" r="1.5" fill="#23242C"/>
+			<circle cx="1.5" cy="7.5" r="1.5" fill="#23242C"/>
+			<circle cx="7.5" cy="1.5" r="1.5" fill="#23242C"/>
+			<circle cx="7.5" cy="7.5" r="1.5" fill="#23242C"/>
+			</g>
+		</svg> 
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
-
-	<?php if ( $blog_info ) : ?>
-		<?php if ( is_front_page() && ! is_paged() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
-		<?php elseif ( is_front_page() && ! is_home() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></h1>
-		<?php else : ?>
-			<p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $blog_info ); ?></a></p>
-		<?php endif; ?>
-	<?php endif; ?>
-
-	<?php if ( $description && true === get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-		<p class="site-description">
-			<?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput ?>
-		</p>
-	<?php endif; ?>
-</div><!-- .site-branding -->
+</div>
