f6000164041b210e9ae69fbe152bc6c5c529818e
mnatkanski <m.natkanski0@gmail.com>
2023-10-11 18:44:35 +0200

    Add info boxes pattern

---
+++ themes/twentytwentyone/inc/patterns/info-boxes/info-boxes.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => \__('Info Boxes'),
+    'categories' => ['custom patterns'],
+    'content' => '<!-- wp:group {"tagName":"section","align":"wide","className":"info-boxes","layout":{"type":"constrained"}} -->
+    <section class="wp-block-group alignwide info-boxes"><!-- wp:columns {"align":"wide"} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:heading -->
+    <h2 class="wp-block-heading">Brands</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum.</p>
+    <!-- /wp:paragraph --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:heading -->
+    <h2 class="wp-block-heading">Styles</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum.</p>
+    <!-- /wp:paragraph --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:heading -->
+    <h2 class="wp-block-heading">Colors</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum.</p>
+    <!-- /wp:paragraph --></div>
+    <!-- /wp:column --></div>
+    <!-- /wp:columns --></section>
+    <!-- /wp:group -->',
+];
