3ebfd7808d7b24370b5618619bddfdda5c733183
mnatkanski <m.natkanski0@gmail.com>
2023-10-11 18:34:24 +0200

    Update footer widgets area

--- themes/twentytwentyone/template-parts/footer/footer-widgets.php
+++ themes/twentytwentyone/template-parts/footer/footer-widgets.php
@@ -9,9 +9,8 @@
 
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
-	<aside class="widget-area">
+	<div class="widget-area">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</aside><!-- .widget-area -->
-
+	</div><!-- .widget-area -->
 	<?php
 endif;
