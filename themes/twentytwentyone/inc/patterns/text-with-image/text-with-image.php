65786929ef66170f4006f27c3fda1ac18aaa806a
mnatkanski <m.natkanski0@gmail.com>
2023-10-11 18:48:30 +0200

    Add text with image pattern

---
+++ themes/twentytwentyone/inc/patterns/text-with-image/text-with-image.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => \__('Text with image'),
+    'categories' => ['custom patterns'],
+    'content' => '<!-- wp:group {"tagName":"section","align":"wide","className":"text-with-image","layout":{"type":"default"}} -->
+    <section class="wp-block-group alignwide text-with-image"><!-- wp:media-text {"align":"wide","mediaId":75,"mediaLink":"http://localhost/?attachment_id=75","mediaType":"image","mediaSizeSlug":"full","imageFill":false} -->
+    <div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="http://localhost/wp-content/uploads/2023/10/Woman-with-hat.jpg" alt="" class="wp-image-75 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"textColor":"tertiary-orange-base"} -->
+    <p class="has-tertiary-orange-base-color has-text-color">Become sexy</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:heading {"textColor":"neutral-extra-dark"} -->
+    <h2 class="wp-block-heading has-neutral-extra-dark-color has-text-color">Mad love <br>for fire headwear</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:buttons -->
+    <div class="wp-block-buttons"><!-- wp:button -->
+    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+    <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div></div>
+    <!-- /wp:media-text --></section>
+    <!-- /wp:group -->',
+];
